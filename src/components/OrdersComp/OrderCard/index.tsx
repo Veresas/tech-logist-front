9f86992a891a5a01f43e16b4c8d0ec8969113c1e
Alexander_Ozhered <Ozhereda@mail.ru>
2025-08-02 12:23:36 +0300

    buckup

--- src/components/OrdersComp/OrderCard/index.tsx
+++ src/components/OrdersComp/OrderCard/index.tsx
@@ -3,7 +3,7 @@ import type { OrderCardProps } from './type';
 
 export const OrderCard = ({ order, onClick }: OrderCardProps) => {
     
-
+//TODO : дописать весь блок
     return (
         <div>
             
