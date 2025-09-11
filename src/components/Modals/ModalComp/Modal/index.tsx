b70f8428fdac42dfbca4f3a3bd25ff0a1c42477a
Alex Oz <ozherda@mail.ru>
2025-09-11 13:58:02 +0300

    stradge commit

--- src/components/Modals/ModalComp/Modal/index.tsx
+++ src/components/Modals/ModalComp/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef} from 'react';
+import { useEffect, useRef} from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
 import type { ModalProps } from './types';
